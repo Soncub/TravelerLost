bd67e586c934fe40b9aa0323d9cbee676938fd00
Andrew Mokhtary <149095320+Soncub@users.noreply.github.com>
2024-10-01 15:22:32 -0400

    Added player and camera controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 input;
+    private CharacterController characterController;
+    private Vector3 direction;
+
+    [SerializeField] private float rotationSpeed = 500f;
+    [SerializeField] private float speed;
+    private Camera mainCamera;
+
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float velocity;
+
+    [SerializeField] private float jumpPower;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        ApplyRotation();
+        ApplyGravity();
+        ApplyMovement();
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene("PrototypeSelection");
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded() && velocity < 0.0f)
+        {
+            velocity = -1.0f;
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        direction.y = velocity;
+    }
+    private void ApplyRotation()
+    {
+        if (input.sqrMagnitude == 0) return;
+        direction = Quaternion.Euler(0.0f, mainCamera.transform.eulerAngles.y, 0.0f) * new Vector3(input.x, 0.0f, input.y);
+        var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+    private void ApplyMovement()
+    {
+        characterController.Move(direction * speed * Time.deltaTime);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        direction = new Vector3(input.x, 0.0f, input.y);
+        Debug.Log(input);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded()) return;
+        velocity += jumpPower;
+    }
+
+    private bool IsGrounded() => characterController.isGrounded;
+}
