4e42ca103fee67679768a8822f9348f290fca625
cjwilson1112 <75537188+wildcardgamez@users.noreply.github.com>
2024-10-05 23:32:04 -0400

    Whistle System Code

--- Assets/Scripts/WhistleSystem.cs
+++ Assets/Scripts/WhistleSystem.cs
@@ -1,15 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class WhistleSystem : MonoBehaviour
 {
-    public GameObject whistleMarker;
+    [Tooltip("UI object to move around for pointing where the whistle should fire")]
+    [SerializeField] private GameObject whistleMarker;
+    [Tooltip("How fast the whistle point should move")]
+    [SerializeField] private float whistleMoveSpeed = 1;
+    [Tooltip("Camera used as a reference for whistling (should be the same one used when whistling)")]
+    [SerializeField] private Camera refCamera;
+    [Tooltip("The furthest you can whistle for the creature to 'follow' it")]
+    [SerializeField] private float travelRange = 10;
+    [Tooltip("The furthest the creature can be from the player to 'hear' it")]
+    [SerializeField] private float listenRange = 10;
 
-    private bool whistleMode;
+    private Vector3 markerAnchorPoint;
+    private Vector2 input;
+    private bool whistling;
 
-    void Update()
+    private CreatureController creature;
+    private PlayerController player;
+
+    private void Start()
     {
-        //WIPs
+        //Set the default position to be where the point starts
+        markerAnchorPoint = whistleMarker.transform.position;
+        //Find the player and creature
+        creature = FindFirstObjectByType<CreatureController>();
+        player = FindFirstObjectByType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        //When whistling, move the pointer based on input
+        if (whistling)
+            whistleMarker.transform.position += (Vector3)input * Time.deltaTime;
+    }
+
+    public void Whistle(InputAction.CallbackContext context)
+    {
+        //When starting to be pressed, start whistling and show the marker at the default position
+        if (context.started)
+        {
+            whistling = true;
+            whistleMarker.transform.position = markerAnchorPoint;
+            whistleMarker.SetActive(true);
+        }
+        //When unpressed, stop whistling and find the point to move the creature to
+        if (context.performed)
+        {
+            whistling = false;
+            whistleMarker.SetActive(false);
+            //Do a raycast from the marker position, then attract the creature if it's in listen+travel range
+            RaycastHit hit;
+            if (Physics.Raycast(refCamera.ScreenPointToRay(whistleMarker.transform.position), out hit) &&
+                Vector3.Distance(player.transform.position, creature.transform.position) < listenRange &&
+                Vector3.Distance(hit.point, creature.transform.position) < travelRange)
+            {
+                creature.NewTargetDestination(hit.point);
+            }
+        }
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
     }
 }
