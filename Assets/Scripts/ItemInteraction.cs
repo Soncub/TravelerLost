54c44e4525330aa8d68a1ca4813191aef0aed52c
paulpie12 <sterscrim@gmail.com>
2024-10-14 22:33:32 -0400

    new input system for item interaction

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ItemInteraction : MonoBehaviour
 {
@@ -13,46 +14,61 @@ public class ItemInteraction : MonoBehaviour
     [Tooltip("Amount of force added when throwing")]
     [SerializeField] private float forceMulti;
 
-    [Tooltip("Visual for Distnace for picking up object")] 
+    [Tooltip("Visual for Distance for picking up object")]
     [SerializeField] private bool enableGizmos;
 
     private bool itemIsPicked;
-
     private Rigidbody rb;
+
+    // Reference to the Input Action
+    [SerializeField] private InputAction pickUpAction;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player").transform;
         pickUpPoint = GameObject.Find("PickUpPoint").transform;
+
+        // Enable the Input Action and subscribe to it
+        pickUpAction.Enable();
+        pickUpAction.performed += PickUp;
     }
 
     private void Update()
     {
+  
+    }
+
+    public void PickUp(InputAction.CallbackContext context)
+    {
+        // Check if the action was performed (button pressed)
+
         // Calculate distance dynamically
         float distanceToPlayer = Vector3.Distance(player.position, transform.position);
-
-        if (distanceToPlayer <= pickUpDistance)
+        if (context.performed && distanceToPlayer <= pickUpDistance && !itemIsPicked && pickUpPoint.childCount < 1)
         {
-            if (Input.GetKeyDown(KeyCode.E) && !itemIsPicked && pickUpPoint.childCount < 1)
-            {
-                rb.useGravity = false;
-                rb.velocity = Vector3.zero; // Stop object movement when picked up
-                this.transform.position = pickUpPoint.position;
-                this.transform.parent = pickUpPoint;
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero; // Stop object movement when picked up
+            this.transform.position = pickUpPoint.position;
+            this.transform.parent = pickUpPoint;
 
-                itemIsPicked = true;
-            }
+            itemIsPicked = true;
+            Debug.Log("Pickup button pressed.");
         }
-
-        if (Input.GetKeyDown(KeyCode.Q) && itemIsPicked)
+        else if (itemIsPicked && context.performed)
         {
             rb.AddForce(player.transform.forward * forceMulti, ForceMode.Impulse);
             this.transform.parent = null;
             rb.useGravity = true;
             itemIsPicked = false;
-
         }
+    }
+
+    private void OnDestroy()
+    {
+        // Disable the Input Action and unsubscribe to prevent memory leaks
+        pickUpAction.Disable();
+        pickUpAction.performed -= PickUp;
     }
 
     private void OnDrawGizmos()
