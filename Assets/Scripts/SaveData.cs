4f155302a19394b3d85a4e8fd5e6e5b51728a79e
Andrew Mokhtary <149095320+Soncub@users.noreply.github.com>
2024-11-22 16:33:47 -0500

    Camera Saving System

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class SaveData
 {
     public float volume = 1f;
+    public float horizontalSensitivity = 75f;
+    public float verticalSensitivity = 75f;
+    public bool invertHorizontal = false;
+    public bool invertVertical = false;
 
     public SaveData()
     {
