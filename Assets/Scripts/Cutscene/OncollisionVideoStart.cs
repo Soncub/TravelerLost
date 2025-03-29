3cf4d6b2f1a6e9f669a8c4a0d52becb47276559c
paulpie12 <sterscrim@gmail.com>
2025-03-28 21:10:40 -0400

    .

--- Assets/Scripts/Cutscene/OncollisionVideoStart.cs
+++ Assets/Scripts/Cutscene/OncollisionVideoStart.cs
@@ -7,7 +7,7 @@ public class OncollisionVideoStart : MonoBehaviour
 public CutsceneManager cutsceneManager;
     private void Start()
     {
-        cutsceneManager.PlayCutscene();
+        
     }
 
     private void OnCollisionEnter(Collision collision)
